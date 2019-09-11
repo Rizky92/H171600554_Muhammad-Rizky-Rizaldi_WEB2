003579b7d429293cc49472c31822204130c718cc
Rizky92 <myquinn489@gmail.com>
2019-09-11 18:43:42 +0800

    Tugas2 login test

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,33 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/hai', function() {
+    return "hello World";
+});
+
+Route::get('/matematika/{a}/{b}', function($a, $b) {
+    return $a + $b;
+});
+
+Route::any('captcha-testimoni', function() {
+    if (request()->getMethod() == 'POST') {
+        $rules = ['captcha' => 'required|captcha'];
+        $validate = validator()->make(request()->all(), $rules);
+
+        if ($validate->fails()) {
+            return "404";
+        }
+        else {
+            return view('welcome');
+        }
+    }
+
+    $form = '<form method="POST" action="captcha-testimoni">';
+    $form .= '<input type="hidden" name="_token" value="'.csrf_token().'">';
+    $form .= '<p>'.captcha_img().'</p>';
+    $form .= '<p><input type="text" name="captcha"></p>';
+    $form .= '<p><button type="submit" name="check">Check</button></p>';
+    $form .= '</form>';
+    return $form;
+});
