867e1132c7ed18e8bb1ce19ea79799115e745f4d
Rizky92 <myquinn489@gmail.com>
2019-10-07 06:27:37 +0800

    Added Edit button

--- resources/views/artikel/index.blade.php
+++ resources/views/artikel/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">{{ __('Articles') }}</div>
 
@@ -16,8 +16,8 @@
                         </div>
 
                         <div class="form-group row">
-                            <table class="table table-striped">
-                                <tr>
+                            <table class="table table-striped table-responsive">
+                                <thead class="thead">
                                     <th>ID</th>
                                     <th>Category</th>
                                     <th>Title</th>
@@ -25,18 +25,21 @@
                                     <th>Created At</th>
                                     <th>Publisher</th>
                                     <th>Action</th>
-                                </tr>
-                                @foreach($artikel as $item)
-                                    <tr>
-                                        <td>{{ $item->id }}</td>
-                                        <td>{{ $item->kategori_artikel->nama }}</td>
-                                        <td>{{ $item->judul }}</td>
-                                        <td>{{ $item->isi }}</td>
-                                        <td>{{ $item->created_at }}</td>
-                                        <td>{{ $item->users->name }}</td>
-                                        <td><a class="btn btn-sm btn-primary" href="{{ route('artikel.show', $item->id)}}">View</a></td>
-                                    </tr>
-                                @endforeach
+                                </thead>
+                                <tbody>
+                                    @foreach($artikel as $item)
+                                        <tr>
+                                            <td>{{ $item->id }}</td>
+                                            <td>{{ $item->kategori_artikel->nama }}</td>
+                                            <td>{{ $item->judul }}</td>
+                                            <td>{{ $item->isi }}</td>
+                                            <td>{{ $item->created_at->format('d-m-Y') }}</td>
+                                            <td>{{ $item->users->name }}</td>
+                                            <td><a class="btn btn-sm btn-primary" href="{{ route('artikel.show', $item->id)}}">View</a><br>
+                                                <a class="btn btn-sm btn-secondary" href="{{ route('artikel.edit', $item->id)}}">Edit</a></td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
                             </table>
                         </div>
                     </form>
