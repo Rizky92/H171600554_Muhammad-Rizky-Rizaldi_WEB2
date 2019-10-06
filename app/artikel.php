afb8eba4e9f85a17f93a2a1c10bb80a27cbcf995
Rizky92 <myquinn489@gmail.com>
2019-10-07 06:25:37 +0800

    Added date Format

--- app/artikel.php
+++ app/artikel.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class artikel extends Model
 {
     protected $fillable = ['judul', 'isi', 'users_id', 'kategori_artikel_id'];
+    protected $dates = ['created_at', 'updated_at'];
     protected $table = 'artikel';
 
     public function kategori_artikel()
