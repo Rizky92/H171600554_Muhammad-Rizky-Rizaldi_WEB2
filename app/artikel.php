6e9782f945a9be598cfd1e1b9657a34550c930a2
Rizky92 <myquinn489@gmail.com>
2019-10-17 12:56:22 +0800

    Re-added soft deletes

--- app/artikel.php
+++ app/artikel.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class artikel extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['judul', 'isi', 'users_id', 'kategori_artikel_id'];
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
     protected $table = 'artikel';
--- app/galeri.php
+++ app/galeri.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class galeri extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['nama', 'keterangan', 'path', 'kategori_galeri_id'];
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
     protected $table = 'galeri';
